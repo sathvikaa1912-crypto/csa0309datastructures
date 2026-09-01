553adfc77af8a59c30761979d1ed74add0ca7cb6
sathvikaa1912-crypto <sathvikaa1912@gmail.com>
2026-09-01 23:12:35 +0530

    Update Experiment12.c

--- 192525224_labExperiment/Experiment12.c
+++ 192525224_labExperiment/Experiment12.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
-#include <ctype.h>
+
+#define MAX 100
 
 int main() {
-    char exp[50], stack[50];
-    int top = -1, i = 0;
+    char exp[MAX], stack[MAX];
+    int top = -1, i;
+    int valid = 1;
 
-    printf("Enter expression: ");
-    scanf("%s", exp);
+    printf("Enter an expression: ");
+    scanf("%99s", exp);
 
-    while (exp[i] != '\0') {
-        if (exp[i] == '(')
-            stack[++top] = exp[i];
+    for (i = 0; exp[i] != '\0'; i++) {
+        if (exp[i] == '(') {
+            top++;
+            stack[top] = exp[i];
+        }
         else if (exp[i] == ')') {
             if (top == -1) {
-                printf("Invalid Expression");
-                return 0;
+                valid = 0;
+                break;
             }
             top--;
         }
-        i++;
     }
 
-    if (top == -1)
+    if (top != -1)
+        valid = 0;
+
+    if (valid == 1)
         printf("Balanced Expression");
     else
-        printf("Invalid Expression");
+        printf("Not Balanced Expression");
 
     return 0;
 }
